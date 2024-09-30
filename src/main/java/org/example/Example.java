1a3e36fe4f746967c307a5262d78f1f2cdea7e44
Matias Navio <naviomatias2@gmail.com>
2024-09-30 18:47:21 -0300

    Initial commit

---
+++ src/main/java/org/example/Example.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Example {
+
+    public int sumar(int n1, int n2){
+
+        return n1 + n2;
+    }
+}
---
+++ src/test/java/org/example/ExampleTest.java
@@ -0,0 +1,29 @@
+package org.example;
+
+
+// se suele importar asi, para no escribir la clase cada vez que la vamos a usar
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+    Example example = new Example();
+
+    @Test
+    public void sumarTest(){
+
+        int result = example.sumar(2, 2); // 4
+
+        // el primer parametró es el valor esperado y el segundo valor es el valor actual
+        assertEquals(4, result);
+    }
+
+    @Test
+    public void sumarTestBoolean(){
+
+        int result = example.sumar(10, 10); // 20
+
+        // esto lo que hace es evaluar si un resultado es verdadero
+        assertTrue(result < 30);
+    }
+}
